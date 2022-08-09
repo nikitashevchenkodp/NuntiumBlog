812a2e4840c8bae864b9369eb3d8d9bff0324815
Nikita Shevchenko <nrcnekita9@gmail.com>
2022-08-09 08:02:38 +0200

    fixed some mistakes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 const search = document.querySelector('#search');
 const trigger = document.querySelector('.find');
 const burger = document.querySelector('.burger_button');
-const nav = document.querySelector('.nav');
+const nav = document.querySelector('.nav__wrapper');
 
 trigger.addEventListener('click', function () {
-    console.log('click');
     search.classList.toggle('input__active');
 })
 
-burger.addEventListener('click', function () {
-    nav.classList.toggle('open')
+burger.addEventListener('click', toggleNav)
+
+function toggleNav() {
+    const status = nav.getAttribute('aria-expanded') === 'true' || false;
+    const isOpen = burger.dataset.opened == 'true' || false;
+    burger.dataset.opened = !isOpen
+    nav.setAttribute('aria-expanded', !status)
+}
+
+nav.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.tagName == 'A' || target.tagName == 'BUTTON') {
+        return
+    } else {
+        nav.setAttribute('aria-expanded', false)
+    }
 })
